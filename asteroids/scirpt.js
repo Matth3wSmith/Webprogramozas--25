823b88a1d3d2057bcecca8a2f7799d04147a0254
Matthew-F4 <kovacs.mate.22@F4-53>
2025-10-14 14:05:25 +0200

    Asteroids

--- asteroids/scirpt.js
+++ asteroids/scirpt.js
@@ -4,12 +4,38 @@ function load(){
     c = document.getElementById("jatek");
     ctx = c.getContext("2d");
 
+    c.addEventListener("onkeydown", gombNyom);
     hajo(100,100);
 }
+function gombNyom(){
+    console.log(this);
+}
+function mozgasW(){
 
-
+}
+function mozgasA(){
+    
+}
+function mozgasS(){
+    
+}
+function mozgasD(){
+    
+}
 function hajo (x,y){
-    ctx.moveTo(x,y);
-    ctx.lineTo(x+100,y+10);
+    ctx.beginPath();
+    //Középső vonal
+    ctx.moveTo(x-20,y);
+    ctx.lineTo(x+20,y);
+
+    //Szögszár
+    ctx.moveTo(x-20,y);
+    ctx.lineTo(x,y-60);
+
+    ctx.moveTo(x+20,y);
+    ctx.lineTo(x,y-60);
+
+
     ctx.stroke();
+    ctx.closePath();
 }
