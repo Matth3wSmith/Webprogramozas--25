afea7fc748b9b29bae8de5543f9b53a2b6ff89c0
unknown <kovacs.mate.22@ady-nagyatad.lan>
2025-11-04 14:05:01 +0100

    asteroids

--- asteroids/scirpt.js
+++ asteroids/scirpt.js
@@ -1,28 +1,54 @@
 var c;
 var ctx;
+let x = 100;
+let y = 100;
+let szog = 90;
 function load(){
     c = document.getElementById("jatek");
     ctx = c.getContext("2d");
 
-    c.addEventListener("onkeydown", gombNyom);
+    document.addEventListener("keydown", function(event){gombNyom(event)});
     hajo(100,100);
 }
-function gombNyom(){
-    console.log(this);
+function torles(){
+    ctx.beginPath();
+    ctx.clearRect(0,0,c.width,c.height);
+    ctx.closePath();
+}
+function gombNyom(e){
+    switch (e.key){
+        case "w":
+            mozgasW();
+            break;
+        case "a":
+            mozgasA();
+            break;
+        case "s":
+            mozgasS();
+            break;
+        case "d":
+            mozgasD();
+            break;
+    }
 }
 function mozgasW(){
-
+    y-=2;
+    hajo()
 }
 function mozgasA(){
-    
+    szog+=5
+    hajo();
 }
 function mozgasS(){
-    
+    y+=2
+    hajo();
 }
 function mozgasD(){
-    
+    szog-=5;
+    hajo();
 }
-function hajo (x,y){
+function hajo (){
+    torles();
     ctx.beginPath();
     //Középső vonal
     ctx.moveTo(x-20,y);
@@ -30,10 +56,10 @@ function hajo (x,y){
 
     //Szögszár
     ctx.moveTo(x-20,y);
-    ctx.lineTo(x,y-60);
+    ctx.lineTo(Math.cos(szog)+x,Math.sin(szog)+(y-60));
 
     ctx.moveTo(x+20,y);
-    ctx.lineTo(x,y-60);
+    ctx.lineTo(Math.cos(szog)+x,Math.sin(szog)+(y-60));
 
 
     ctx.stroke();
